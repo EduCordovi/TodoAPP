2b7da99fbac09921232fa777ea625f512c03ace6
EduCordovi <educordovi@gmail.com>
2024-06-15 18:15:13 -0400

    Editar y Borrar las Tareas

--- app/src/main/java/com/educordovi/todoapp/addtasks/ui/TaskScreen.kt
+++ app/src/main/java/com/educordovi/todoapp/addtasks/ui/TaskScreen.kt
@@ -1,6 +1,7 @@
 package com.educordovi.todoapp.addtasks.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -29,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -72,7 +74,11 @@ fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
     Card(
         Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)//, elevation = 9.dp
+            .padding(horizontal = 16.dp, vertical = 8.dp).pointerInput(Unit){
+                detectTapGestures (onLongPress = {
+                    tasksViewModel.onItemRemove(taskModel)
+                })
+            }//, elevation = 9.dp
     ) {
         Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
             Text(
